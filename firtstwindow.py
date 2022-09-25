88dd545dd00cbe03e075f17af12675b9ff5cccda
Auc1Lem <slavapuska9@gmail.com>
2022-09-25 19:04:41 +0300

    finish first window , add second and third Window

--- firtstwindow.py
+++ firtstwindow.py
@@ -1,15 +1,42 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import  QApplication, QWidget, QLabel , QPushButton , QVBoxLayout, QHBoxLayout
 from instr import *
+from secondWindow import *
 
 class MainWindow (QWidget):
     def __init__(self):
         super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
         self.show()
 
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height,)
+        self.move(win_y, win_x)
+
+    def initUI (self):
+        self.lb_hello = QLabel(txt_hello)
+        self.lb_instruction = QLabel(txt_instruction)
+        self.btn_next = QPushButton(txt_next)
+
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(self.lb_hello,alignment= Qt.AlignCenter)
+        self.v_line.addWidget(self.lb_instruction,alignment= Qt.AlignCenter)
+        self.v_line.addWidget(self.btn_next , alignment= Qt.AlignCenter)
+        
+        self.setLayout(self.v_line)
+    
+    def connects (self) :
+        self.btn_next.clicked.connect(self.next_Window)
+
+    def next_Window(self): 
+        self.hide()
+        self.sec_win = SecondWindow()
 
 
-
+    
 app =  QApplication([])
 MainWindow = MainWindow()
 app.exec_()
